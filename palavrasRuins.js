a1ec1b6fa0d686615384b736d2acfd7ae55ba0f5
joaomelo250809 <goncalves.melo.joao@escola.pr.gov.br>
2025-07-05 13:52:21 -0300

    Update palavrasRuins.js

--- palavrasRuins.js
+++ palavrasRuins.js
@@ -1,1 +1,9 @@
-
+export const PALAVRAS_RUINS = new Set([
+  "que", "para", "com", "não", "uma", "por", "mais", "dos", "como", "mas",
+  "foi", "ele", "das", "tem", "seu", "sua", "ser", "quando", "muito", "está",
+  "também", "pelo", "pela", "até", "isso", "ela", "entre", "era", "depois",
+  "sem", "mesmo", "aos", "ter", "seus", "quem", "nas", "esse", "eles", "estão",
+  "você", "tinha", "foram", "essa", "num", "nem", "suas", "meu", "minha", "têm",
+  "numa", "pelos", "elas", "havia", "seja", "qual", "será", "nós", "tenho", "lhe",
+  "deles", "essas", "esses", "pelas", "este", "fosse", "dele", "vocês", "vos",
+  "lhes", "meus", 
